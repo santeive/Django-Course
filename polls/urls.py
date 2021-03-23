01f5aff0c7e8e2452bdc51b1daf691e710ad00a6
santeive <packo.ive@gmail.com>
2021-03-22 18:02:54 -0600

    generic views

--- polls/urls.py
+++ polls/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 # Agregamos app_name para diferenciar las vistas entre apps, 
@@ -7,11 +8,11 @@ app_name = 'polls'
 
 urlpatterns = [
      # ex: /polls/
-    path('', views.index, name="index"),
+    path('', views.IndexView.as_view(), name='index'),
     # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
